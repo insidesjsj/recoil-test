892b1ca4f863b5104d49dfcac51f4a263594a8c4
insidesjsj <tjdwns428@naver.com>
2024-05-03 01:20:43 +0900

    4. selector + get selector : 다른 state를 파생할 수 있음 get 함수로 countState 값 가져옴 read-only 속성 / write 불가능
    하지만 set 속성 추가시 가능

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,11 +1,12 @@
 import {FC} from 'react';
 import {useRecoilState, useRecoilValue, useResetRecoilState, useSetRecoilState} from 'recoil';
-import {countState} from './recoilState';
+import {countState, everOrOddState} from './recoilState';
 
 const Counter: FC = () => {
     const count = useRecoilValue(countState)
     const setCount = useSetRecoilState(countState)
     const resetCount = useResetRecoilState(countState)
+    const evenOrOdd = useRecoilValue(everOrOddState)
 
     const increment = () => {
         setCount(count + 1)
@@ -20,6 +21,7 @@ const Counter: FC = () => {
     return (
         <div>
             <p>count: {count}</p>
+            <p>Even or Odd : {evenOrOdd}</p> {/* 파생 상태 출력 */}
             <button onClick={increment}>Increment</button>
             <button onClick={reset}>Reset</button>
             <button onClick={decrement}>Decrement</button>
